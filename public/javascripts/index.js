b0df3183fb77bf4d9bdc21eee5b9d0459ab51d77
willf <will.fitzgerald@gmail.com>
2012-03-10 19:40:22 -0800

    Update public/javascripts/index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   
   window.wordnik = new Api({
     discoveryUrl: "http://api.wordnik.com/v4/resources.json",
-    apiKey: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+    apiKey: "YOUR_API_KEY",
     success: function() {  }
   });
      
